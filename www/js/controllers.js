f399ccb2f981346fbc0e1ec75beb76e3b052a8b1
Ioan Eugen Stan <ieugen@apache.org>
2015-02-01 23:48:48 +0200

    Initial import

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('starter.controllers', []).controller('PiuiGenCtrl', function ($scope) {
+    $scope.pw = {};
+
+    $scope.update = function () {
+        var service = $scope.pw.service;
+        master = Crypto.SHA256($scope.pw.master);
+
+        var pass = Crypto.MD5(service + master);
+        pass = service.toUpperCase().charAt(0) + pass.substring(1, 3) + '%' + pass.substring(4, 6) + "#" + pass.substring(7, 12);
+
+        $scope.pw.password = pass
+    }
+
+});
